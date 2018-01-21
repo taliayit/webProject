ea9699dcec84d453238808f9b8588c09393e3b04
givolaj <givolaj@gmail.com>
2018-01-21 19:34:05 +0200

    timer:D

--- AutoLotto/Scripts/modalScript.js
+++ AutoLotto/Scripts/modalScript.js
@@ -4,22 +4,25 @@
     var imgpath = "../Content/images/exercises/" + options.name;
     $("#curr_exercise").attr("src", imgpath);
 
-    $("#workoutModal").find(".modalStopwatch").TimeCircles({
-        start: true, count_past_zero: false,
-        time: {
-            Days: { show: false },
-            Hours: { show: false },
-            Minutes: { show: false },
-            Seconds: { show: true, color: "#c55" }
-        },
-        total_duration: 90
-    });
+    $("#workoutModal").modal("toggle");
+
+    setTimeout(function () {
+        $("#workoutModal").find(".modalStopwatch").TimeCircles({
+            start: true, count_past_zero: false,
+            time: {
+                Days: { show: false },
+                Hours: { show: false },
+                Minutes: { show: false },
+                Seconds: { show: true, color: "#c55" }
+            },
+            total_duration: 90
+        });
+    }, 300);
+    
    // $(".start").click(function () { $(".example.stopwatch").TimeCircles().start(); });
     //$(".stop").click(function () { $(".example.stopwatch").TimeCircles().stop(); });
     //$(".restart").click(function () { $(".example.stopwatch").TimeCircles().restart(); });
     //$(".start").click();
-
-    $("#workoutModal").modal("toggle");
 }
 
 
